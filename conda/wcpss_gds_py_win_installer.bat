12aac5503a4e6103a2792c2a5db4e266c83dedfe
Ryan Cooper <ryancooper729@gmail.com>
2022-03-16 13:55:17 -0400

    initial commit

---
+++ conda/wcpss_gds_py_win_installer.bat
@@ -0,0 +1,15 @@
+REM Please run this from the Anaconda Prompt
+REM This installer is pinned to the latest released version
+REM Set up GDS environment
+call conda env create -f https://github.com/wcpss-geo/wcpss_gds_py_env/raw/conda/gds_py.yml
+REM Activate environment
+call conda activate wcpss_gds
+REM Add pip packages
+call conda install -y -c conda-forge git
+pip install --user -r https://github.com/wcpss-geo/wcpss_gds_py_env/raw/conda/gds_py_pip.txt
+REM JupyterLab old plugin's
+jupyter labextension install nbdime-jupyterlab --no-build
+jupyter lab build -y
+jupyter lab clean -y
+conda clean --all -f -y
+call conda deactivate
