70edaab3d7e3de3f022bf1b757ee1b44553d0769
Ryan Cooper <ryancooper729@gmail.com>
2022-03-18 08:30:15 -0400

    update URLs to config files

--- wcpss_gds_py_win_installer.bat
+++ wcpss_gds_py_win_installer.bat
@@ -1,12 +1,12 @@
 REM Please run this from the Anaconda Prompt
 REM This installer is pinned to the latest released version
 REM Set up GDS environment
-call conda env create -f https://raw.githubusercontent.com/wcpss-geo/wcpss_gds_py_env/main/conda/wcpss_gds_py.yml
+call conda env create -f https://raw.githubusercontent.com/wcpss-geo/wcpss_gds_py_env/main/wcpss_gds_py.yml
 REM Activate environment
 call conda activate wcpss_gds
 REM Add pip packages
 call conda install -y -c conda-forge git
-pip install --user -r https://raw.githubusercontent.com/wcpss-geo/wcpss_gds_py_env/main/conda/wcpss_gds_py_pip.txt
+pip install --user -r https://raw.githubusercontent.com/wcpss-geo/wcpss_gds_py_env/main/wcpss_gds_py_pip.txt
 REM JupyterLab old plugin's
 jupyter labextension install nbdime-jupyterlab --no-build
 jupyter lab build -y
